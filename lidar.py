148dabbc0e0ff4b4c8dfa76997f7f5b026897cff
met4000 <nathsharwor@hotmail.com>
2024-08-11 17:15:55 +0800

    remove extra files

--- lidar.py
+++
@@ -1,3 +0,0 @@
-
-from eye import LIDARGet as _LIDARGet
-from eye import LIDARSet as _LIDARSet
--- psd.py
+++
@@ -1,5 +0,0 @@
-
-from eye import PSD_FRONT, PSD_LEFT, PSD_RIGHT, PSD_BACK
-
-from eye import PSDGet as _PSDGet
-from eye import PSDGetRaw as _PSDGetRaw
